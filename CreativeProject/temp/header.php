0a2f70d3fe36653b5ce5a48ab399544e8de88a5a
Raymond G McCord <raymond@selfinteractive.com>
2020-07-25 23:49:49 -0500

    init header

---
+++ CreativeProject/temp/header.php
@@ -0,0 +1,17 @@
+<head>
+    <title>Interactive Media Arts Gallery</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Student Gallery</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add Student</a></li>
+            </ul>
+        </div>
+    </nav>
