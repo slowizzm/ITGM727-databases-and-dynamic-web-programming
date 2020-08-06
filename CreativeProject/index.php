2319f0a4cfd5aef0e8e915f3fc0ec3cf27dad2c7
Raymond G McCord <raymond@selfinteractive.com>
2020-08-05 23:50:41 -0500

    optimize - update form

--- CreativeProject/index.php
+++ CreativeProject/index.php
@@ -1,44 +1,34 @@
 <?php
-
-include 'conn.php';
-$username = $_POST['username'];
+session_start();
+include 'includes/conn.php';
+$username  = $_POST['username'];
 $password = $_POST['password'];
-$query = "SELECT *FROM users WHERE username='$username' AND password='$password'";
+$query = "SELECT *FROM student_info WHERE username='$username' AND password='$password'";
 $result = mysqli_query($conn, $query);
-$row = mysqli_fetch_array($result);
+$row = mysqli_fetch_array($result, MYSQLI_NUM);
 $isLoggedIn = false;
+$_SESSION['id'] = $row[0];
 
 //error list
 $errs = array('username' => '', 'password' => '');
 
 if ($row > 1) {
-    if ($row[3] == 0) {
+    if ($row[4] == 0) {
         header('location: admin.php');
-    } else {
+    } else if ($row[4] == 1) {
         header('location: gallery.php?isLoggedIn=true;');
+    } else {
+        echo 'err: you are not an admin or user';
     }
 } else {
     //check errs and redirect user
     if (array_filter($errs)) {
-       echo 'err';
+        echo 'err';
     } else {
         // header('Location: results.php');
         // echo "<script> alert('username and password do not match');</script>";
     }
 }
-
-//validation
-if (isset($_POST['submit'])) {
-    //validate username
-    if (empty($_POST['username'])) {
-        $errs['username'] = 'req username';
-    } else {
-        $username = $_POST['username'];
-        if (!preg_match('/^[a-zA-Z\s]+$/', trim($username))) {
-            $errs['username'] = 'must use letters and spaces only';
-        }
-    }
-} // end of validation
 ?>
 
 <!DOCTYPE html>
@@ -46,26 +36,26 @@ if (isset($_POST['submit'])) {
 
 <head>
     <title>Interactive Media Arts Gallery</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 
 <body>
-    <section class="container grey-text" style="margin-top: 120px">
-        <h4 class="center">LOGIN</h4>
-        <form class="white" action="index.php" method="POST">
-            <label>Username</label>
-            <input type="text" name="username" value="<?php echo htmlspecialchars($username) ?>">
-            <div class=" red-text"><?php echo $errs['username']; ?></div>
-            <label>Password:</label>
-            <input type="password" name="password" value="">
-            <div class="red-text"><?php echo $errs['password']; ?></div>
-            <div class="center" style="margin-top: 20px">
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-            </div>
-        </form>
-    </section>
+
+    <div class="container">
+        <!-- FORM -->
+        <form class="form" action="index.php" method="POST">
+
+            <input type="text" name="username" value="<?php echo htmlspecialchars($username) ?>" placeholder="Name" required>
+
+            <input type="password" name="password" value="" required>
+
+            <input type="submit" name="submit" value="submit" class="btn">
+            <a href="">Lost your password?</a>
+        </form> <!-- END OF FORM -->
+    </div>
+
+    
+
 </body>
 
 </html>
