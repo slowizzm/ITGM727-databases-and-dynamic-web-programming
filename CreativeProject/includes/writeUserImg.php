ec5425139eda3c00f55c866a22922a0f7228982e
Raymond G McCord <raymond@selfinteractive.com>
2020-08-05 23:51:05 -0500

    init commit

---
+++ CreativeProject/includes/writeUserImg.php
@@ -0,0 +1,24 @@
+<?php
+
+function writeUserImg($dir, $img)
+{
+    // temp name
+    $img_tmp = $img['tmp_name'];
+echo 'hhhhheeeeee';
+    //add timestamp
+    $img_name = getTimestamp() . $img['name'];
+
+    //strip whitespace from string
+    $img_name = str_replace(' ', '', $img_name);
+
+    // set img path
+    $img = $dir . '/' . $img_name;
+
+    // move to img path
+    if (move_uploaded_file($img_tmp, $img) == FALSE) {
+    } else {
+        chmod($img, 0644);
+    }
+
+    return $img;
+}
