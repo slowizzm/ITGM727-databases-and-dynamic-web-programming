15a45f12f37478c02b254e5d89ee612dbc0f63f8
Raymond G McCord <raymond@selfinteractive.com>
2020-08-08 20:38:55 -0500

    update data inputs - style tweaks

--- CreativeProject/profile.php
+++ CreativeProject/profile.php
@@ -20,15 +20,18 @@ $row = mysqli_fetch_array($result, MYSQLI_NUM);
 
 //init user data
 $user_id = $row[0];
-$email = $row[1];
-$username = $row[2];
-$password = $row[3];
+$email = $row[2];
+$username = $row[3];
+$password = $row[4];
 $major = $row[5];
-$semester = $row[6];
-$course = $row[7];
-$section = $row[8];
-$img = $row[10];
-$url = $row[11];
+$artistic_influence = $row[6];
+$bio = $row[7];
+$img = $row[8];
+$url = $row[9];
+$semester = $row[10];
+$course_id = $row[11];
+$section = $row[12];
+
 
 
 if (isset($_POST['submit_pw'])) {
@@ -51,20 +54,18 @@ if (isset($_POST['submit_data'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $major = mysqli_real_escape_string($conn, $_POST['major']);
-    $semester = mysqli_real_escape_string($conn, $_POST['semester']);
-    $course = mysqli_real_escape_string($conn, $_POST['course_id']);
-    $section = mysqli_real_escape_string($conn, $_POST['section']);
+    $artistic_influence = mysqli_real_escape_string($conn, $_POST['artistic_influence']);
+    $bio = mysqli_real_escape_string($conn, $_POST['bio']);
     $url = mysqli_real_escape_string($conn, $_POST['p5_url']);
 
     //update sql
     $sql = "UPDATE student_info 
     SET username='$username'
-    ,major='$major'
-    ,semester='$semester'
-    ,course_id='$course'
-    ,section='$section'
-    ,p5_url='$url'
     ,email='$email'
+    ,major='$major'
+    ,artistic_influence='$artistic_influence'
+    ,bio='$bio'
+    ,p5_url='$url'
      WHERE id='$user_id'";
     //save to db
     if (mysqli_query($conn, $sql)) {
@@ -74,7 +75,7 @@ if (isset($_POST['submit_data'])) {
     $_SESSION['user'] = $username;
 }
 
-if (isset($_POST['submit_userImg'])) {
+if (isset($_FILES['upload_userImg'])) {
 
     // store date and time
     $date .= date('n/d/Y');
@@ -92,9 +93,9 @@ if (isset($_POST['submit_userImg'])) {
 
 
     // check if guest uploaded img
-    if ($_FILES['userImg'] && $_FILES['userImg']['size'] > 0) {
+    if ($_FILES['upload_userImg'] && $_FILES['upload_userImg']['size'] > 0) {
         // store img
-        $imgUpload = $_FILES['userImg'];
+        $imgUpload = $_FILES['upload_userImg'];
         // print_r($imgUpload);
 
         // save img
@@ -135,8 +136,8 @@ if (isset($_POST['submit_userImg'])) {
 
 <head>
     <title>Interactive Media Arts Gallery</title>
-    <link rel="stylesheet" type="text/css" href="css/profile.css">
     <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/profile.css">
 </head>
 
 <body>
@@ -153,14 +154,15 @@ if (isset($_POST['submit_userImg'])) {
                     <div class="user-row">
                         <div class="user-img-container">
                             <div class="img-container" style="width: 140px;">
-                                <img src="<?php echo $img ?>" class="img" style="height: 140px; background-color: rgb(233, 236, 239);"> <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">140x140</span></img>
+                                <img src="<?php echo $img ?>" class="img"> <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">140x140</span></img>
                                 <div class="img-upload">
                                     <div class="form-group file-upload">
                                         <form action="profile.php" method="post" enctype="multipart/form-data">
-                                            <label class="file-upload-text" for="guestImg">Upload Photo</label>
-                                            <input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
-                                            <input type="file" accept=".png, .jpg, .jpeg" class="input-file" name="userImg">
-                                            <div class="col save-wrap"> <button class="btn-save" type="submit" name="submit_userImg">Save Changes</button></div>
+                                            <div class="img-change-wrap">
+                                                <input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
+                                                <input type="file" accept=".png, .jpg, .jpeg" class="input-file" name="upload_userImg" onchange="this.form.submit()" />
+                                                <button class="btn-file-upload" type="" name="submit_userImg">Change Photo</button>
+                                            </div>
                                         </form>
                                     </div>
                                 </div><!-- END OF IMG-UPLOAD -->
@@ -170,79 +172,65 @@ if (isset($_POST['submit_userImg'])) {
                         <div class="user-info-wrap">
                             <div class="user-info">
                                 <h4 class="username"><?php echo $username ?></h4>
+                                <ul class="course-info">
+                                    <li class="course-item"><?php echo 'Semester: ' . $semester ?></li>
+                                    <li class="course-item"><?php echo 'Course ID: ' . $course_id ?></li>
+                                    <li class="course-item"><?php echo 'Section: ' . $section ?></li>
+                                </ul>
+
                             </div>
                         </div><!-- END OF USER-INFO-WRAP -->
                     </div><!-- END OF USER-ROW -->
 
+                    <div class="forms">
 
-                    <!-- USER DATA -->
+                        <!-- USER DATA -->
+                        <form class="form-data" action="profile.php" method="post" enctype="multipart/form-data">
+                            <div class="user-input-container">
+                                <div class="user-input-row">
+                                    <div class="user-input-col">
+                                        <div class="col">
+                                            <div class="form-group"> <label>Name</label> <input class="form-control" type="text" name="username" value="<?php echo $username ?>" required style="background-image: none !important;"></div>
+                                            <div class="form-group"> <label>Major</label> <input class="form-control" type="text" name="major" placeholder="Major" value="<?php echo $major ?>" required></div>
+                                            <div class="form-group"> <label>Artistic Influence</label> <input class="form-control" type="text" name="artistic_influence" placeholder="What artist has influenced" value="<?php echo $artistic_influence ?>" required></div>
+                                            <div class="form-group"> <label>Bio</label><textarea id="text-area" class="form-control" rows="4" cols="50" maxlength="125" name="bio" placeholder="Short bio" required><?php echo $bio ?></textarea></div>
+                                            <div><span id="chars">100</span> characters remaining</div>
+                                        </div>
+                                        <button class="btn-save" type="submit" name="submit_data">Save Changes</button>
+                                    </div><!-- END OF USER-INPUT-ROW -->
+                                </div><!-- END OF USER-INPUT-CONTAINER -->
+                        </form> <!-- END OF FORM-DATA -->
 
-                    <form class="form" action="profile.php" method="post" enctype="multipart/form-data">
-                        <div class="user-input-container">
-                            <div class="user-input-row">
+
+                        <!-- VALIDATE PASSWORD -->
+
+
+                        <form class="form-pw" name="frmChange" method="post" action="" onsubmit="return validatePassword()">
+                            <div class="user-pw-container">
                                 <div class="user-input-col">
+                                    <div id="message" class="message"><?php if (isset($message)) {
+                                                                            echo $message;
+                                                                        } ?></div>
+                                    <div class="password"><b>Change Password</b></div>
                                     <div class="col">
-                                        <div class="form-group"> <label>Name</label> <input class="form-control" type="text" name="username" value="<?php echo $username ?>" required style="background-image: url(); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;"></div>
+                                        <div class="form-group pw"> <label>Current Password</label> <input class="form-control" type="password" name="currentPassword" placeholder="*****" required></div>
                                     </div>
                                     <div class="col">
-                                        <div class="form-group"> <label>Major</label> <input class="form-control" type="text" name="major" placeholder="Major" value="<?php echo $major ?>" required></div>
+                                        <div class="form-group pw"> <label>New Password</label> <input class="form-control" type="password" name="newPassword" placeholder="*****" required></div>
                                     </div>
                                     <div class="col">
-                                        <div class="form-group"> <label>Semester</label> <input class="form-control" type="text" name="semester" placeholder="Current Semester" value="<?php echo $semester ?>" required></div>
+                                        <div class="form-group pw"> <label id="confirmPassword">Confirm Password</label> <input class="form-control" type="password" name="confirmPassword" placeholder="*****" required></div>
                                     </div>
                                 </div><!-- END OF USER-INPUT-COL -->
-                                <div class="user-input-col">
-                                    <div class="col">
-                                        <div class="form-group"> <label>Course</label> <input class="form-control" type="text" name="course_id" placeholder="Course Id" value="<?php echo $course ?>" required></div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="form-group"> <label>Section</label> <input class="form-control" type="text" name="section" placeholder="Course Section" value="<?php echo $section ?>" required></div>
-                                    </div>
-                                    <div class=" col">
-                                        <div class="form-group"> <label>P5.js URL</label> <input class="form-control" type="text" name="p5_url" placeholder="P5.js Sketches url" value="<?php echo $url ?>" required></div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="form-group"> <label>Email</label> <input class="form-control" type="text" name="email" placeholder="email" value="<?php echo $email ?>" required></div>
-                                    </div>
-                                </div><!-- END OF COL -->
-                            </div><!-- END OF USER-INPUT-ROW -->
-                            <div class="row">
-                                <div class="col save-wrap"> <button class="btn-save" type="submit" name="submit_data">Save Changes</button></div>
-                            </div>
-                    </form> <!-- END OF FORM -->
-                </div><!-- END OF USER-INPUT-CONTAINER -->
+                                <button class="btn-save" type="submit" name="submit_pw">Save Password</button>
+                            </div><!-- END OF USER-PW-CONTAINER -->
+                        </form><!-- END OF FORM_PW -->
 
+                    </div><!-- END OF FORMS -->
+                </div><!-- END OF CARD-BODY -->
+            </div><!-- END OF CARD -->
 
-                <!-- VALIDATE PASSWORD -->
-
-
-                <form name="frmChange" method="post" action="" onsubmit="return validatePassword()">
-                    <div class="user-pw-container">
-                        <div class="user-input-col">
-                            <div id="message" class="message"><?php if (isset($message)) {
-                                                                    echo $message;
-                                                                } ?></div>
-                            <div class="password"><b>Change Password</b></div>
-                            <div class="col">
-                                <div class="form-group pw"> <label>Current Password</label> <input class="form-control" type="password" name="currentPassword" placeholder="*****" required></div>
-                            </div>
-                            <div class="col">
-                                <div class="form-group pw"> <label>New Password</label> <input class="form-control" type="password" name="newPassword" placeholder="*****" required></div>
-                            </div>
-                            <div class="col">
-                                <div class="form-group pw"> <label id="confirmPassword">Confirm Password</label> <input class="form-control" type="password" name="confirmPassword" placeholder="*****" required></div>
-                            </div>
-                        </div><!-- END OF COL -->
-                    </div>
-                    <div class="col save-wrap"> <button class="btn-save" type="submit" name="submit_pw">Save Changes</button></div>
-                </form>
-
-
-            </div><!-- END OF CARD-BODY -->
-        </div><!-- END OF CARD -->
-
-    </div><!-- END OF CARD-CONTAINER -->
-
+        </div><!-- END OF CARD-CONTAINER -->
     </div><!-- END OF CONTAINER -->
 
 
@@ -264,6 +252,21 @@ if (isset($_POST['submit_userImg'])) {
 
             return output;
         }
+
+        let maxLength = 125;
+        var el;
+
+        function countCharacters(e) {
+            var textEntered, countRemaining, counter;
+            textEntered = document.getElementById('text-area').value;
+            counter = (maxLength - (textEntered.length));
+            countRemaining = document.getElementById('chars');
+            countRemaining.textContent = counter;
+        }
+        el = document.getElementById('text-area');
+        el.addEventListener('keyup', countCharacters, false);
+
+        countCharacters();
     </script>
 
 </body>
