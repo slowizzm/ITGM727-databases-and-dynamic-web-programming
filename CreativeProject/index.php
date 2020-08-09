59453e9af84b3cabc5eadb7fea5ca837cdf8267f
Raymond G McCord <raymond@selfinteractive.com>
2020-08-08 20:37:59 -0500

    optimize nav_options - update data array

--- CreativeProject/index.php
+++ CreativeProject/index.php
@@ -9,17 +9,17 @@ $row = mysqli_fetch_array($result, MYSQLI_NUM);
 
 //sessions vars
 $_SESSION['id'] = $row[0];
-$_SESSION['user'] = $row[2];
-$_SESSION['img'] = $row[10];
+$_SESSION['user'] = $row[3];
+$_SESSION['img'] = $row[8];
 $_SESSION['isLoggedIn'] = false;
 
 //error list
 $errs = array('username' => '', 'password' => '');
 
 if ($row > 1) {
-    if ($row[4] == 0) {
+    if ($row[1] == 0) {
         header('location: admin.php');
-    } else if ($row[4] == 1) {
+    } else if ($row[1] == 1) {
         $_SESSION['isLoggedIn'] = true;
         header('location: gallery.php');
     } else {
@@ -54,8 +54,8 @@ if ($row > 1) {
 
             <input type="password" name="password" value="" required>
 
-            <input type="submit" name="submit" value="submit" class="btn">
-            <a href="">Lost your password?</a>
+            <input type="submit" name="submit" value="LOGIN" class="btn">
+            <p class="nav_options">Don't have an account? <a href="signup.php" class="signup-btn">Signup</a></p>
         </form> <!-- END OF FORM -->
     </div>
 
