1902e7101cec5f7071e0f0dd3b1627da45e7372c
Raymond G McCord <raymond@selfinteractive.com>
2020-08-23 00:37:54 -0500

    updates active status, resets pw, removes user - needs active status css work

--- CreativeProject/admin.php
+++ CreativeProject/admin.php
@@ -3,17 +3,81 @@ session_start();
 //connect to db
 include 'includes/conn.php';
 //query users
-$query = 'SELECT id, email, username, major, artistic_influence, bio, img, p5_url, semester, course_id, section FROM student_info ORDER BY created_at';
+$query = 'SELECT id, email, username, major, artistic_influence, bio, img, p5_url, semester, course_id, section, isActive FROM students ORDER BY created_at';
 
 //get result
 $result = mysqli_query($conn, $query);
-
 //fetch results
 $students = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+$init = $_SESSION['has_init'];
+
+
+foreach ($students as $key => $stud) {
+    if ($init != 'init') {
+        $opacityActiveStatus[$key] = 'opaque';
+        $cardColorActiveStatus[$key] = 'card-active';
+        $btnDeactivateStatus[$key] = 'show';
+        $btnReactivateStatus[$key] = 'hidden';
+        $tester = $_SESSION['has_init'];
+        $_SESSION['has_init'] = 'init';
+    }
+
+    if (isset($_POST['deactivate-' . $key])) {
+        $tester = 'innactive key ' . $key;
+        $opacityActiveStatus[$key] = 'no-opaque';
+        $cardColorActiveStatus[$key] = 'card-inactive';
+        $btnDeactivateStatus[$key] = 'hidden';
+        $btnReactivateStatus[$key] = 'show';
+        $sql = "UPDATE students 
+    SET isActive='0'
+     WHERE id='$key'";
+        //save to db
+        if (mysqli_query($conn, $sql)) {
+        } else {
+            echo 'query err: ' . mysqli_error($conn);
+        }
+    } else if (isset($_POST['reactivate-' . $key])) {
+        $tester = 'active key ' . $key;
+        $opacityActiveStatus[$key] = 'opaque';
+        $cardColorActiveStatus[$key] = 'card-active';
+        $btnDeactivateStatus[$key] = 'show';
+        $btnReactivateStatus[$key] = 'hidden';
+        $sql = "UPDATE students 
+    SET isActive='1'
+     WHERE id='$key'";
+        //save to db
+        if (mysqli_query($conn, $sql)) {
+        } else {
+            echo 'query err: ' . mysqli_error($conn);
+        }
+    }
+
+    if (isset($_POST['remove-' . $key])) {
+        $sql = "DELETE FROM students WHERE id='$key'";
+        mysqli_query($conn, $sql);
+        header('location: admin.php');
+    }
+
+    if (isset($_POST['reset-' . $key])) {
+        $sql = "UPDATE students 
+    SET password='newPW290'
+     WHERE id='$key'";
+        //save to db
+        if (mysqli_query($conn, $sql)) {
+        } else {
+            echo 'query err: ' . mysqli_error($conn);
+        }
+    }
+}
 
 //free mem / close conn
 mysqli_free_result($result);
 mysqli_close($conn);
+
+// echo $reactivate;
+
 ?>
 
 <!-- HTML -->
@@ -30,7 +94,11 @@ mysqli_close($conn);
 
 <body>
     <?php include 'includes/header.php'; ?>
+
+
+
     <div class="admin-container">
+        <div style="font-size: 2rem"><?php echo $tester ?></div>
         <div class="admin-panel-container">
             <div class="panel-card-info">
                 <div class="panel-title student-info">Student Info</div>
@@ -38,8 +106,8 @@ mysqli_close($conn);
                 <div class="panel-title student-code">Code Info</div>
                 <div class="panel-title student-password">Reset Password</div>
             </div>
-
-            <?php foreach ($students as $student) : ?>
+            <!-- $array as $key =>> $element -->
+            <?php foreach ($students as $key => $student) : ?>
                 <?php
                 $user_name = htmlspecialchars($student['username']);
                 $user_email = htmlspecialchars($student['email']);
@@ -53,8 +121,8 @@ mysqli_close($conn);
                 $section = htmlspecialchars($student['section']);
                 ?>
 
-                <div class="card">
-                    <div class="card-student-info-wrapper">
+                <div class="card <?php echo $cardColorActiveStatus[$key] ?> ">
+                    <div class="card-student-info-wrapper student-info-opaque <?php echo $opacityActiveStatus[$key] ?>">
                         <img class="card-avatar" src="<?php echo $img ?>" alt="<?php echo htmlspecialchars($student['username']) ?>" />
                         <div class="card-student-info-container">
                             <p class="card-info-email"><?php echo $user_email ?></p>
@@ -66,28 +134,45 @@ mysqli_close($conn);
                         </div>
                     </div>
 
-                    <div class="card-student-name-container">
+                    <div class="card-student-name-container student-name-opaque <?php echo $opacityActiveStatus[$key] ?>">
                         <p class="card-student-name"><?php echo $user_name ?></p>
                         <p class="card-major"><?php echo $major ?></p>
                     </div>
 
-                    <div class="card-code-info-container">
-                        <p class="card-code-info-link"><a href="<?php echo $url ?>"><?php echo $user_name ?>'s Sketches</a> </p> 
+                    <div class="card-code-info-container student-code-opaque <?php echo $opacityActiveStatus[$key] ?>">
+                        <p class="card-code-info-link"><a href="<?php echo $url ?>"><?php echo $user_name ?>'s Sketches</a> </p>
                         <p class="card-code-info-fav-artist">Favorite Artist: <?php echo $artistic_influence ?></p>
                     </div>
 
                     <div class="card-options-container">
-                        <button class="card-btn">Reset</button>
-                        <div class="card-more">
-                            <span class="card-more-circ"></span>
-                            <span class="card-more-circ"></span>
-                            <span class="card-more-circ"></span>
-                        </div>
+                        <form action="admin.php" method="post">
+                            <button class="card-btn btn-opaque <?php echo $opacityActiveStatus[$key] ?>" name="<?php echo 'reset-' . $key ?>" type="submit">Reset</button>
+                            </form>
+                            <div class=" admin-dropdown" id="admin-dropdown">
+                                <button class="card-more">
+                                    <span class="card-more-circ"></span>
+                                    <span class="card-more-circ"></span>
+                                    <span class="card-more-circ"></span>
+                                </button>
+                                <div class="admin-dropdown-content">
+                                    <form action="admin.php" method="post">
+                                        <button class="user-active-status user-active <?php echo $btnDeactivateStatus[$key] ?>" name="<?php echo 'deactivate-' . $key ?>" type="submit">Deactivate</button>
+                                    </form>
+                                    <form action="admin.php" method="post">
+                                        <button class="user-active-status user-inactive <?php echo $btnReactivateStatus[$key] ?>" name="<?php echo 'reactivate-' . $key ?>" type="submit">Reactivate</button>
+                                    </form>
+                                    <form action="admin.php" method="post">
+                                        <button class="user-active-status user-remove" name="<?php echo 'remove-' . $key ?>" type="submit">Remove User</button>
+                                    </form>
+                                </div>
+                            </div>
                     </div>
                 </div>
             <?php endforeach ?>
         </div>
     </div>
+
+    <script src="js/helpers.js"></script>
 </body>
 
 </html>
