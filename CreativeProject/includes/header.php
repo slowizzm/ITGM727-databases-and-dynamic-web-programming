a5411c93e79ffc90f54cb86aa866d36c2c98163c
Raymond G McCord <raymond@selfinteractive.com>
2020-08-23 00:40:11 -0500

    update for admin/student

--- CreativeProject/includes/header.php
+++ CreativeProject/includes/header.php
@@ -1,9 +1,9 @@
 <?php
 
-$isLoggedIn = $_SESSION['isLoggedIn'];
+$isAdminLoggedIn = $_SESSION['isAdminLoggedIn'];
+$isStudentLoggedIn = $_SESSION['isStudentLoggedIn'];
 $nav_img = $_SESSION['img'];
 $user_name = $_SESSION['user'];
-$isLoggedIn = $_SESSION['isLoggedIn'];
 ?>
 
 <!-- TODO - Setup logout -->
@@ -13,7 +13,20 @@ $isLoggedIn = $_SESSION['isLoggedIn'];
         <a href="gallery.php" class="btn brand" style="text-transform: uppercase">Interactive Media Design Arts</a>
         <div class="user-nav">
             <div class="btn-wrapper">
-                <?php if ($isLoggedIn == true) {
+                <?php
+                if ($isAdminLoggedIn == true) {
+                    echo  '<img src="' . $nav_img . '" class="usrimg">';
+                    echo '<div class="dropdown">
+    <button class="dropbtn">' . $user_name . ' 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="admin-profile.php">Edit Profile</a>
+      <a href="index.php">Log Out</a>
+    </div>
+  </div> ';
+                    // echo '<a href="profile.php" class="btn" style="text-transform: uppercase">' . $user_name . '</a>';
+                } else if ($isStudentLoggedIn == true) {
                     echo  '<img src="' . $nav_img . '" class="usrimg">';
                     echo '<div class="dropdown">
     <button class="dropbtn">' . $user_name . ' 
@@ -24,11 +37,11 @@ $isLoggedIn = $_SESSION['isLoggedIn'];
       <a href="index.php">Log Out</a>
     </div>
   </div> ';
-                    // echo '<a href="profile.php" class="btn" style="text-transform: uppercase">' . $user_name . '</a>';
                 } else {
                     echo '<a href="login.php" class="btn" style="text-transform: uppercase">Login</a>';
                     echo '<a href="signup.php" class="btn">SIGN UP</a>';
-                } ?>
+                }
+                ?>
             </div>
         </div>
     </div>
