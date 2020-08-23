13db5e8158ebc81de7a43f1fc80a94f930493b85
Raymond G McCord <raymond@selfinteractive.com>
2020-08-23 00:41:45 -0500

    remove unused - destroy session

--- CreativeProject/login.php
+++ CreativeProject/login.php
@@ -3,19 +3,8 @@
 include 'includes/conn.php';
 include 'includes/query_users.php';
 
-print_r($row);
+session_destroy();
 
-if($row>1){
-    if($row[3] == 0) {
-        header('location: admin.php');
-        // echo 'isAdmin';
-    } else {
-        header('location: gallery.php');
-        // echo 'isUser';
-    }
-
-} else {
     // echo "<script> alert('username and password do not match');</script>";
     header('location: index.php');
-}
 ?>
