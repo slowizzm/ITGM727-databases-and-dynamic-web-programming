8b35ade458fbd13c8eeffda0c1325a37dfb8c597
Raymond G McCord <raymond@selfinteractive.com>
2020-08-24 22:17:11 -0500

    clean up - comment

--- CreativeProject/js/helpers.js
+++ CreativeProject/js/helpers.js
@@ -1,21 +1,24 @@
+//event listeners - more, deactivate, reactivate buttons to update styles on hover/click
 const cardDropDowns = document.querySelectorAll('.card-more');
-const cards = document.querySelectorAll('.card');
-const cardActive = document.querySelectorAll('.card-active');
 const activeStatus = document.querySelectorAll('.user-active');
 const inactiveStatus = document.querySelectorAll('.user-inactive');
 
+//styles to update
+const cards = document.querySelectorAll('.card');
+const cardActive = document.querySelectorAll('.card-active');
 const infoOpaque = document.querySelectorAll('.student-info-opaque');
 const nameOpaque = document.querySelectorAll('.student-name-opaque');
 const codeOpaque = document.querySelectorAll('.student-code-opaque');
 const btnOpaque = document.querySelectorAll('.btn-opaque');
-
 const dropContents = document.querySelectorAll('.admin-dropdown-content');
 
+//add event listener to all buttons on card
 for (let i = 0; i < cards.length; i++) {
     cardDropDowns[i].addEventListener('click', _ => {
         dropContents[i].classList.toggle('show');
     });
 
+    //toggle style on deactivate click
     activeStatus[i].addEventListener('click', _ => {
         cardActive[i].classList.toggle('card-inactive');
         inactiveStatus[i].classList.toggle('show');
@@ -30,6 +33,7 @@ for (let i = 0; i < cards.length; i++) {
         btnOpaque[i].classList.add('no-opaque');
     });
 
+    //toggles styles on reactive click
     inactiveStatus[i].addEventListener('click', _ => {
         cardActive[i].classList.toggle('card-inactive');
         inactiveStatus[i].classList.toggle('show');
