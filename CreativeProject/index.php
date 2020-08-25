8f93edbc9663e18673f5ef4425d97c908975acf6
Raymond G McCord <raymond@selfinteractive.com>
2020-08-24 22:13:21 -0500

    clean up

--- CreativeProject/index.php
+++ CreativeProject/index.php
@@ -1,23 +1,30 @@
 <?php
 session_start();
 include 'includes/conn.php';
+
+//connect to database and query results depending on if admin or student
 $username  = $_POST['username'];
 $password = $_POST['password'];
 $admin_query = "SELECT *FROM admins WHERE username='$username' AND password='$password'";
 $student_query = "SELECT *FROM students WHERE username='$username' AND password='$password'";
+
+//set row for admin
 $admin_result = mysqli_query($conn, $admin_query);
 $admin_row = mysqli_fetch_array($admin_result, MYSQLI_NUM);
 
+//set row for student
 $student_result = mysqli_query($conn, $student_query);
 $student_row = mysqli_fetch_array($student_result, MYSQLI_NUM);
 
 //sessions vars
 $_SESSION['isAdminLoggedIn'] = false;
 $_SESSION['isStudentLoggedIn'] = false;
+$_SESSION['has_init'] = false;
 
 //error list
 $errs = array('username' => '', 'password' => '');
 
+//check if admin or student logged in - set sessoion vars accordingly and redirect
 if ($admin_row > 1) {
     $_SESSION['id'] = $admin_row[0];
     $_SESSION['user'] = $admin_row[2];
@@ -25,7 +32,8 @@ if ($admin_row > 1) {
 
     $_SESSION['isAdminLoggedIn'] = true;
     header('location: admin.php');
-} else if ($student_row > 1) {
+}
+if ($student_row > 1) {
     $_SESSION['id'] = $student_row[0];
     $_SESSION['user'] = $student_row[2];
     $_SESSION['img'] = $student_row[7];
@@ -50,7 +58,7 @@ if ($admin_row > 1) {
 </head>
 
 <body>
-
+    <!-- LOGIN FORM -->
     <div class="container">
         <!-- FORM -->
         <form class="form" action="index.php" method="POST">
